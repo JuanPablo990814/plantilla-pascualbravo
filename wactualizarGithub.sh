aa3da1eb5b53c7b6ec5f8a36efe7d8a89cb01d6a
juanpablo990814 <juanpablopb99@gmail.com>
2021-04-21 22:39:07 -0500

    Primera Nota

---
+++ wactualizarGithub.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "Primera Nota"
+git push -u origin master
---
+++ winicialGithub.sh
@@ -0,0 +1,4 @@
+git init
+git config --global user.email "juanpablopb99@gmail.com"
+git config --global user.name "juanpablo990814"
+git remote add origin https://github.com/JuanPablo990814/plantilla-pascualbravo.git
